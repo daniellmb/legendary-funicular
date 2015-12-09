21193397e492b2b60eb047d28b9a4e49167ea0e4
Daniel Lamb <daniellmb@hotmail.com>
2015-12-09 12:21:38 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# legendary-funicular
-Legendary Funicular
+# Legendary Funicular
+
+### [Live Demo](http://codepen.io/daniellmb/pen/eJNBzr?editors=001)
+
+## Programming Game
+
+  1. Create a new repository on GitHub.
+  1. Use the suggested repository name.
+  1. Write code that does what the repository name suggests.
---
+++ funicular.js
@@ -0,0 +1,43 @@
+(function(c) {
+    function rounded(x, y, width, height, radius) {
+        c.beginPath();
+        c.moveTo(x + radius, y);
+        c.lineTo(x + width - radius, y);
+        c.quadraticCurveTo(x + width, y, x + width, y + radius);
+        c.lineTo(x + width, y + height - radius);
+        c.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+        c.lineTo(x + radius, y + height);
+        c.quadraticCurveTo(x, y + height, x, y + height - radius);
+        c.lineTo(x, y + radius);
+        c.quadraticCurveTo(x, y, x + radius, y);
+        c.closePath();
+        c.fill();
+    }
+    // rail
+    c.beginPath();
+    c.moveTo(111, 17);
+    c.lineTo(111, 13);
+    c.lineTo(8, 52);
+    c.lineTo(8, 56);
+    c.closePath();
+    c.fill();
+    // left wheel
+    rounded(48, 20, 10, 10, 6);
+    // right wheel
+    rounded(62, 14, 10, 10, 6);
+    // post
+    c.beginPath();
+    c.moveTo(60, 60);
+    c.lineTo(60, 42);
+    c.lineTo(65, 40);
+    c.lineTo(65, 60);
+    c.closePath();
+    c.fill();
+    // basket
+    rounded(31, 60, 61, 45, 5);
+    // windows
+    c.fillStyle = "#fff";
+    rounded(34, 66, 15, 15, 2);
+    rounded(54, 66, 15, 15, 2);
+    rounded(74, 66, 15, 15, 2);
+}(document.querySelector('canvas').getContext('2d')));
